3c9bff1b80ce8a32821245d6a93fa0511f9c1a84
cmng <pentncm@gmail.com>
2020-06-08 17:37:28 +0800

    Add a cpu_constraint check

--- all_checks.py
+++ all_checks.py
@@ -21,10 +21,15 @@ def check_root_full():
     """Returns True if the root partition is full, False otherwise."""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
+def check_cpu_constrained():
+    """Returns True if the cpu is having too much usage, False otherwise."""
+    return psetil.cpu_percent(1)> 75
+
 def main():
     checks=[
         (check_reboot, "Pending Reboot"),
         (check_root_full, "Root partition full"),
+        (check_cpu_constrained, "CPU too much")
     ]
     everything_ok = True
     for check, msg in checks:
